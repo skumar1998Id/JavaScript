60935296b173c454976803e328219e8f84fbea8a
SantoshKumar <santosh.k@ascentbusiness.com>
2023-07-13 11:10:35 +0530

    Added Something

--- test/src/main.js
+++ test/src/main.js
@@ -3,13 +3,18 @@ function firstFunction(){
 }
 
 const user = {firstName: 'Santosh', lastName:'Kumar', age: '23', gender: 'Male',
-    fullName : function (){
+    fullName(){
     return this.firstName + " " + this.lastName
     }
 }
+
+function fullName(firstName, lastName) {
+    return `${firstName} ${lastName}`
+}
+
 function getUserDetails(){
     document.getElementById("user").innerHTML=user.name + " " +user.gender + " " + user.age
-    document.getElementById("user").innerHTML=user.fullName();
+    document.getElementById("user").innerHTML=fullName("Santosh", "kumar");
 
 }
 
